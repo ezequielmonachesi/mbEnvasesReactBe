f6d17dd47fdf3552de0074e165800988ed712586
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2024-01-07 16:08:50 -0300

    se termina de configurar middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 // tomar puerto
 //crear una instancia de express
@@ -9,6 +10,9 @@ app.listen(app.get("port"), () => {
   console.log("Estoy en el puerto " + app.get("port"));
 });
 
-//middleware
+//middleware: funciones que se ejecutan antes de las rutas
+app.use(cors()); //permite conexiones remotas
+app.use(express.json()); // puedo tomar del objeto request de  datos formato json.
+
 
 //rutas
