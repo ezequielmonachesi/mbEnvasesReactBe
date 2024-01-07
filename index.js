adf66566031d61fc1c32e774efae064e07909a89
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2024-01-07 16:05:29 -0300

    agregue puerto en la configuracion del index

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import express from "express";
 
-console.log("tercer prueba del backend sip");
+// tomar puerto
+//crear una instancia de express
+const app = express();
+//crear una variable para guardar puerto
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto " + app.get("port"));
+});
+
+//middleware
+
+//rutas
